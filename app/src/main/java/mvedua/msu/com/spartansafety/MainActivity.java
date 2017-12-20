30ba60e872fcb13d614e774edca334a71b0efeb3
Matthew Vedua <veduamat@msu.edu>
2017-12-19 21:18:12 -0500

    Initial Commit

---
+++ app/src/main/java/mvedua/msu/com/spartansafety/MainActivity.java
@@ -0,0 +1,44 @@
+package mvedua.msu.com.spartansafety;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_resources:
+                    mTextMessage.setText(R.string.title_resources);
+                    return true;
+                case R.id.navigation_report:
+                    mTextMessage.setText(R.string.title_report);
+                    return true;
+                case R.id.navigation_twitter:
+                    mTextMessage.setText(R.string.title_twitter);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
